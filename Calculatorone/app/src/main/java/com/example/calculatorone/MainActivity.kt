12e65d908fbc62234ef6731195e23f22ac3b169c
omonterroso-2022141 <omonterroso-2022141@kinal.edu.gt>
2024-08-23 11:21:31 -0600

    feat: Modified the activity_main in res/layout file
    - Added a TextView to observe calculations and operations.
    - The activity_main file and buttons_layout were joined together to form the base is of the calculator.
    - Added changeOperator function to be able to identify logical
      operators.
    - Added the calculate function that gives logic to the different
      operations.
    - Added selectionNumber function to be able to select a number and see
      itself reflected in the calculator.

--- Calculatorone/app/src/main/java/com/example/calculatorone/MainActivity.kt
+++ Calculatorone/app/src/main/java/com/example/calculatorone/MainActivity.kt
@@ -1,20 +1,87 @@
 package com.example.calculatorone
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+        val add = "+"
+        val subtract = "-"
+        val multiply = "*"
+        val divide = "/"
+        val porcent = "%"
+
+    var operation = ""
+    var numberOne:Double = Double.NaN
+    var numberTwo:Double = Double.NaN
+
+    lateinit var temporal:TextView
+    lateinit var result:TextView
+    lateinit var decimal:DecimalFormat
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        decimal = DecimalFormat("#.#####")
+        temporal = findViewById(R.id.temporal)
+        result = findViewById(R.id.result)
+    }
+
+    fun changeOperator(b: View) {
+        if(temporal.text.isNotEmpty() || numberOne.toString()!="NaN"){
+
+        calculate()
+        val boton: Button = b as Button
+        if (boton.text.toString().trim() == "÷") {
+            operation = "/"
+        } else if (boton.text.toString().trim() == "x") {
+            operation = "*"
+        } else {
+            operation = boton.text.toString().trim()
+        }
+        result.text = decimal.format(numberOne) + operation
+        temporal.text = ""
+
         }
     }
+
+    fun calculate() {
+        try {
+            if (numberOne.toString()!="NaN") {
+                if(temporal.text.toString().isEmpty()){
+                    temporal.text = result.text.toString()
+                }
+                numberTwo = temporal.text.toString().toDouble()
+                temporal.text = ""
+
+                when (operation) {
+                    "+" -> numberOne = (numberOne + numberTwo)
+                    "-" -> numberOne = (numberOne - numberTwo)
+                    "*" -> numberOne = (numberOne * numberTwo)
+                    "/" -> numberOne = (numberOne / numberTwo)
+                    "%" -> numberOne = (numberOne * (numberTwo / 100))
+                }
+            } else {
+                numberOne = temporal.text.toString().toDouble()
+            }
+        }catch (e:Exception){
+
+        }
+    }
+
+    fun selectionNumber(b: View){
+        val boton:Button = b as Button
+        if(temporal.text.toString()=="0"){
+            temporal.text=""
+        }
+        temporal.text = temporal.text.toString() + boton.text.toString()
+    }
+
+
 }
