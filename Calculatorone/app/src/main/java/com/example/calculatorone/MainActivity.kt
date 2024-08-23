2cd0133d76248ffb9117119484f3665033514fe0
omonterroso-2022141 <omonterroso-2022141@kinal.edu.gt>
2024-08-23 11:21:31 -0600

    feat: Modified the activity_main in res/layout file
    - Added a TextView to observe calculations and operations.
    - The activity_main file and buttons_layout were joined together to form the basis of the calculator.

--- Calculatorone/app/src/main/java/com/example/calculatorone/MainActivity.kt
+++ Calculatorone/app/src/main/java/com/example/calculatorone/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.calculatorone
 
+import android.icu.text.DecimalFormat
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
-    }
+
+        val add = "+"
+        val subtract = "-"
+        val multiply = "*"
+        val divide = "/"
+        val porcent = "%"
+
 }
